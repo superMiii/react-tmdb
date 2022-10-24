eccfb1f91587b0e4427cbd63010b2ca80d805458
superMiii <fahminurfalah9@gmail.com>
2022-10-24 14:48:24 +0700

    fix import unused vars

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Pages/Login";
-import LogoutButton from "./components/LogoutButton";
 import { Routes, Route, Link, Navigate } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
-import Profile from "./Pages/Profile";
 import Home from "./Pages/Home";
-import { app } from "./firebase";
 import {
   getAuth,
   signInWithEmailAndPassword,
